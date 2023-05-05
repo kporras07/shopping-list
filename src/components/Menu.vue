ff73f6727d79bb8b647b57a3c89ab2cd2b82bdf9
Kevin Porras <kporras07@gmail.com>
2023-05-04 21:35:41 -0600

    Coding standards.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="menu">
-      <h2>Menu</h2>
-      <ul>
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/target-stock">Manage Target Stock</router-link>
-        </li>
-        <li>
-          <router-link to="/current-stock">Manage Current Stock</router-link>
-        </li>
-        <li>
-          <router-link to="/shopping-list">Get Shopping List</router-link>
-        </li>
-      </ul>
-    </div>
-  </template>
+  <div class="menu">
+    <h2>Menu</h2>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/target-stock">Manage Target Stock</router-link>
+      </li>
+      <li>
+        <router-link to="/current-stock">Manage Current Stock</router-link>
+      </li>
+      <li>
+        <router-link to="/shopping-list">Get Shopping List</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
 
-  <style>
+<style>
 ul {
   list-style-type: none;
   padding: 0;
@@ -31,4 +31,3 @@ a {
   color: #42b983;
 }
 </style>
-  
--- src/components/TargetStock.vue
+++ src/components/TargetStock.vue
@@ -1,40 +1,39 @@
 <template>
-    <div class="target-stock">
-      <h1>{{ intro }}</h1>
-      <Menu />
-    </div>
-  </template>
-  
-  <script>
-  import Menu from './Menu.vue'
-  export default {
-    name: 'TargetStock',
-    components: {
-      Menu
-    },
-    data () {
-      return {
-        intro: 'Manage Target Stock'
-      }
+  <div class="target-stock">
+    <h1>{{ intro }}</h1>
+    <Menu />
+  </div>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+export default {
+  name: 'TargetStock',
+  components: {
+    Menu
+  },
+  data () {
+    return {
+      intro: 'Manage Target Stock'
     }
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
-  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
